e68f86f2a77304c1f4a54beefaedb78ed1d502cc
feliposz <feliposz@gmail.com>
2024-04-03 06:07:02 -0300

    Read a blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"bufio"
+	"compress/zlib"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		printUsageAndExit()
+		printUsageAndExit("")
 	}
 
 	switch os.Args[1] {
 	case "init":
 		gitInit()
+	case "cat-file":
+		gitCatFile()
 	default:
 		fmt.Printf("invalid command: %s\n", os.Args[1])
-		printUsageAndExit()
+		printUsageAndExit("")
 	}
 }
 
-func printUsageAndExit() {
-	fmt.Printf("usage: %s <command> [<args>...]", filepath.Base(os.Args[0]))
+func printUsageAndExit(command string) {
+	myName := filepath.Base(os.Args[0])
+	if len(command) == 0 {
+		fmt.Printf("usage: %s <command> [<args>...]", myName)
+	} else {
+		fmt.Printf("usage: %s %s", myName, command)
+	}
 	os.Exit(1)
+}
+
+func fatal(msg string, args ...any) {
+	fmt.Printf(msg, args...)
+	os.Exit(128)
 }
 
 func gitInit() {
@@ -30,18 +45,91 @@ func gitInit() {
 	for _, directory := range initialDirectories {
 		err := os.Mkdir(directory, 0755)
 		if err != nil {
-			fmt.Printf("error creating directory %s: %s", directory, err)
-			os.Exit(1)
+			fatal("error creating directory %s: %s", directory, err)
 		}
 	}
 	headPath := ".git/HEAD"
 	headContent := "ref: refs/heads/master\n"
 	err := os.WriteFile(headPath, []byte(headContent), 0644)
 	if err != nil {
-		fmt.Printf("error writing to file %s: %s", headPath, err)
-		os.Exit(1)
+		fatal("error writing to file %s: %s", headPath, err)
 	}
 	cwd, _ := os.Getwd()
 	repository := filepath.Join(cwd, ".git")
 	fmt.Printf("Initialized empty Git repository in %s\n", repository)
+}
+
+func gitCatFile() {
+	if len(os.Args) < 4 || !(os.Args[2] == "-p" || os.Args[2] == "-t" || os.Args[2] == "-s" || os.Args[2] == "-e") {
+		printUsageAndExit("cat-file (-p | -t | -s | -e) <object>")
+	}
+
+	objName := os.Args[3]
+	if len(objName) != 40 {
+		fatal("fatal: Not a valid object name %s\n", objName)
+	}
+
+	objDir := filepath.Join(".git", "objects", objName[:2])
+	info, err := os.Stat(objDir)
+	if err != nil {
+		fatal(err.Error())
+	}
+	if !info.IsDir() {
+		fatal("fatal: not a directory %s\n", objDir)
+	}
+
+	objPath := filepath.Join(objDir, objName[2:])
+	file, err := os.Open(objPath)
+	if err != nil {
+		fatal(err.Error())
+	}
+	defer file.Close()
+
+	if os.Args[2] == "-e" { // only check if object exists
+		os.Exit(0)
+	}
+
+	zipReader, err := zlib.NewReader(file)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	reader := bufio.NewReader(zipReader)
+	objType, _ := reader.ReadString(' ')
+	objType = objType[:len(objType)-1]
+
+	if os.Args[2] == "-t" {
+		fmt.Println(objType)
+		return
+	}
+
+	if objType != "blob" {
+		fatal("object type not supported: %q\n", objType)
+	}
+
+	lengthStr, err := reader.ReadString(0)
+	lengthStr = lengthStr[:len(lengthStr)-1]
+	if err != nil {
+		fatal(err.Error())
+	}
+	objSize, _ := strconv.ParseInt(lengthStr, 10, 64)
+
+	if os.Args[2] == "-s" {
+		fmt.Println(objSize)
+		return
+	}
+
+	if objSize == 0 {
+		fatal("error: object file %s is empty", objPath)
+	}
+
+	buffer := make([]byte, objSize)
+	_, err = reader.Read(buffer)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	// default action "-p" (pretty-print)
+
+	os.Stdout.Write(buffer)
 }
