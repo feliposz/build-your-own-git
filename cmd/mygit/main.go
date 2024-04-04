4308aa98832dec3eb3c8ce8c5ee03482e3fcc2bc
feliposz <feliposz@gmail.com>
2024-04-04 02:34:49 -0300

    Create a blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -19,6 +20,8 @@ func main() {
 		gitInit()
 	case "cat-file":
 		gitCatFile()
+	case "hash-object":
+		gitHashObject()
 	default:
 		fmt.Printf("invalid command: %s\n", os.Args[1])
 		printUsageAndExit("")
@@ -132,4 +135,73 @@ func gitCatFile() {
 	// default action "-p" (pretty-print)
 
 	os.Stdout.Write(buffer)
+}
+
+func gitHashObject() {
+	if len(os.Args) < 3 || (os.Args[2] == "-w" && len(os.Args) < 4) {
+		printUsageAndExit("hash-object -w <object>")
+	}
+
+	var writeObject bool
+	var filename string
+	if os.Args[2] == "-w" {
+		filename = os.Args[3]
+		writeObject = true
+	} else {
+		filename = os.Args[2]
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fatal(err.Error())
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		fatal(err.Error())
+	}
+	if info.IsDir() {
+		fatal("'%s' is a directory", info.Name())
+	}
+	fileSize := info.Size()
+
+	payload := []byte(fmt.Sprintf("blob %d\000", fileSize))
+	content := make([]byte, fileSize)
+	_, err = file.Read(content)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	s := sha1.New()
+	s.Write(payload)
+	s.Write(content)
+
+	objName := fmt.Sprintf("%x", s.Sum(nil))
+
+	if !writeObject {
+		fmt.Println(objName)
+		return
+	}
+
+	objDir := objName[:2]
+	err = os.MkdirAll(filepath.Join(".git", "objects", objDir), 0755)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	objFile, err := os.OpenFile(filepath.Join(".git", "objects", objDir, objName[2:]), os.O_CREATE, 0644)
+	if err != nil {
+		fatal(err.Error())
+	}
+	defer objFile.Close()
+	writer := zlib.NewWriter(objFile)
+	writer.Write(payload)
+	writer.Write(content)
+	err = writer.Close()
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	fmt.Println(objName)
 }
